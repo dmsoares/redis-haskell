3f2d3dc8ef46a5d94be06fdb5a548b351688ba48
Decio Soares <decio.msoares@gmail.com>
2026-08-19 21:59:49 +0100

    codecrafters submit [skip ci]

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NumericUnderscores #-}
 
 module Main (main) where
 
 import Network.Simple.TCP (serve, HostPreference(HostAny), closeSock, recv, send, Socket)
 import System.IO (hPutStrLn, hSetBuffering, stdout, stderr, BufferMode(NoBuffering))
 import Data.ByteString as BS
+import Control.Monad (forever, forM)
 
 main :: IO ()
 main = do
@@ -22,12 +25,14 @@ main = do
     putStrLn $ "Redis server listening on port " ++ port
     serve HostAny port $ \(socket, address) -> do
         putStrLn $ "successfully connected client: " ++ show address
-        mBytes <- recv socket 32
-        maybeReply socket mBytes
+
+        forever $ do
+            mBytes <- recv socket 14
+            maybeReply socket mBytes
+
         closeSock socket
 
 maybeReply :: Socket -> Maybe BS.ByteString -> IO ()
 maybeReply socket Nothing = putStrLn "no data received"
-maybeReply socket (Just bytes) = do
-    putStrLn $ "received: " ++ (show bytes)
-    send socket $ "+PONG\r\n"
+maybeReply socket (Just "*1\r\n$4\r\nPING\r\n") = send socket $ "+PONG\r\n"
+maybeReply socket (Just bytes) = putStrLn $ "received: " <> (show bytes)
