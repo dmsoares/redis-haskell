08d680fa3f8f027ca9a09ca42e6358a0701a46f8
Decio Soares <decio.msoares@gmail.com>
2026-09-24 00:35:04 +0100

    add nix flake with devShell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "codecrafters-redis-haskell dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            stack
+            ghc
+            haskell-language-server
+            zlib
+            pkg-config
+          ];
+
+          # Stack manages its own GHC/package snapshot per stack.yaml;
+          # keep it from also reaching for Nix so the two don't fight.
+          STACK_YAML = "stack.yaml";
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.zlib}/lib:$LD_LIBRARY_PATH"
+          '';
+        };
+      });
+}
